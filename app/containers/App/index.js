5d645e171e37c412c585f8cb213c8fc0de91e692
Loui Zibdawi <lzibdawi@mail.uoguelph.ca>
2019-03-04 18:47:44 -0500

    Initial react template

---
+++ app/containers/App/index.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * App
+ *
+ * This component is the skeleton around the actual pages, and should only
+ * contain code that should be seen on all pages. (e.g. navigation bar)
+ */
+
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from 'containers/HomePage';
+import NotFoundPage from 'containers/NotFoundPage';
+
+import './style.scss';
+
+const App = () => (
+  <div className="app-wrapper">
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="" component={NotFoundPage} />
+    </Switch>
+  </div>
+);
+
+export default App;

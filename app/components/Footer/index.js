ff78f8bd756073904a3216919189d86a6b996538
Loui Zibdawi <lzibdawi@mail.uoguelph.ca>
2019-03-08 04:08:04 -0500

    Updated readme

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -4,8 +4,8 @@ import './styles.scss';
 const Footer = () => (
   <footer className="footer-distributed">
     <div className="footer-right">
-      <a href="#"><i className="fa fa-linkedin"></i></a>
-      <a href="#"><i className="fa fa-github"></i></a>
+      <a href="https://www.linkedin.com/in/loui-zibdawi-885186124/" target="_blank"><i className="fa fa-linkedin"></i></a>
+      <a href="https://github.com/LouiZibdawi/TriangleTest" target="_blank"><i className="fa fa-github"></i></a>
     </div>
     <div className="footer-left">
       <p>Loui Zibdawi &copy; 2019</p>

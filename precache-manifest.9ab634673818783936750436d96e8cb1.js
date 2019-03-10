36ff6f8ccad4b6be82d17ef28c6f76865c40b072
Loui Zibdawi <lzibdawi@mail.uoguelph.ca>
2019-03-10 04:06:54 -0400

    Updates

---
+++ precache-manifest.9ab634673818783936750436d96e8cb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba768f646609cf7c5bd7",
+    "url": "/TriangleTest/static/js/runtime~main.29b5e3c3.js"
+  },
+  {
+    "revision": "2fe314251ca6f6296503",
+    "url": "/TriangleTest/static/js/main.0b0ccb94.chunk.js"
+  },
+  {
+    "revision": "2ba16fbdc45797f5643c",
+    "url": "/TriangleTest/static/js/2.f7bf9ff1.chunk.js"
+  },
+  {
+    "revision": "2fe314251ca6f6296503",
+    "url": "/TriangleTest/static/css/main.40a50efb.chunk.css"
+  },
+  {
+    "revision": "bff743d8b3637b3c49a2ebb06239aad2",
+    "url": "/TriangleTest/index.html"
+  }
+];
